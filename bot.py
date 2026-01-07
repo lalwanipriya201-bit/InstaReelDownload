b6623e9341ec047e801a867f054a0287e197db82
Priya Lalwani <lalwanipriya201@gmail.com>
2026-01-07 22:59:05 +0530

    Replaced the instaloader with alternative yt-dlp

--- bot.py
+++ bot.py
@@ -1,19 +1,19 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-import instaloader
 import os
 import shutil
 import uuid
+import time
+import logging
+import yt_dlp
+
 
 
 BOT_TOKEN="8319252236:AAGrDnC3X_AjeDlE1zq7ZJxPU9zZwCVTuLc"
 
-loader=instaloader.Instaloader(
-    download_videos=True,
-    download_video_thumbnails=False,
-    save_metadata=False,
-    post_metadata_txt_pattern=""
-)
+
+
+
 
 
 async def start(update:Update, context:ContextTypes.DEFAULT_TYPE ):
@@ -29,25 +29,48 @@ async def handle_message(update:Update, context:ContextTypes.DEFAULT_TYPE):
         return
     
     await update.message.reply_text("⏳ Downloading reel, please wait...")
+    
+    
+    folder=f"reel_{uuid.uuid4().hex}"
+    os.makedirs(folder, exist_ok=True)
+
+    ydl_opts={
+        "format":"mp4",
+        "outtmpl":f"{folder}/%(id)s.%(ext)s",
+        "quiet":True,
+        "noplaylist":True,
+        "no_warnings":True,
+    }
+
+    video_sent=False
+    
+    
 
     try:
-        shortcode=url.split("/")[-2]
-        post=instaloader.Post.from_shortcode(loader.context,shortcode)
 
-        folder=f"reel_{uuid.uuid4().hex}"
-        loader.download_post(post, target=folder)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        
 
+        
+    except Exception as e:
+        await update.message.reply_text(
+        f"⚠️ Failed to download reel.\nError: {e}"
+        )
+
+    if os.path.exists(folder):
         for file in os.listdir(folder):
             if file.endswith(".mp4"):
                 video_path = os.path.join(folder, file)
                 with open(video_path, "rb") as video:
                     await update.message.reply_video(video=video)
+                video_sent = True
                 break
+    shutil.rmtree(folder, ignore_errors=True)
 
-    except Exception as e:
-        await update.message.reply_text(
-        f"⚠️ Failed to download reel.\nError: {str(e)}"
-        )
+    if not video_sent:
+        await update.message.reply_text("⚠️ Failed to download reel.")
+
 
 app=ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
