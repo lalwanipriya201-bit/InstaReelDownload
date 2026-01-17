2e1f253c392f41a3e5da130b5a9b138cd40f9c19
lalwanipriya201-bit <lalwanipriya201@gmail.com>
2026-01-17 19:37:45 +0530

    update

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def handle_message(update:Update, context:ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("⚠️ Failed to download reel.")
 
 
-app=ApplicationBuilder().token(BOT_TOKEN).build()
+app=ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
